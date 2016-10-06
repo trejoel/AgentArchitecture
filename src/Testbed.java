1d959c99726c3d08b4cfe75c1901d3249a05a4d0
trejoel <trejoel@users.noreply.github.com>
2016-10-06 18:34:52 -0500

    Add files via upload

--- src/Testbed.java
+++ src/Testbed.java
@@ -1,12 +1,16 @@
 
 import java.util.Hashtable;
 
-public class Testbed {
+public class Testbed extends Thread {
 	//See the specifications of the VMAs in page 12 of the cluster computing journal
+	
+	private int simulationRun=0;
+    private String dataCenterIP;	
 	
 	private SMA[] SMA_low=new SMA[6];
 	private SMA[] SMA_medium=new SMA[8];
 	private SMA[] SMA_high=new SMA[6];
+	private VMA[] virtual_machine=new VMA[300];
 	private FA front_agent=new FA();
 	private WorkloadGenerator myWorkload;
 	//private Hashtable <Integer, SMA> SMAS; //This is maybe not required, we can access to the 
@@ -14,8 +18,10 @@ public class Testbed {
 	
 	private int startTime;
 	
-	public Testbed(int xstartTime){
+	public Testbed(int xstartTime, String dataCenterIP, int simulationRun){
         this.startTime=xstartTime;
+        this.dataCenterIP = dataCenterIP;
+        this.simulationRun=simulationRun;        
 	}
 	
 	public int getStartingTime(){
@@ -24,19 +30,6 @@ public class Testbed {
 	
 	public void start(){   // Start the FA and the SMAs
 
-		
-		//Here comes a workloadgenerator object
-		
-		/*for (int i=0;i<300;i++){
-			xCPU_Avaible=generateRandomCPU();
-			xMEM_Avaible=generateRandomMEM();
-			xstart_time=xstart_time+generateRandomStartTime();			
-			xexecution_time= generateRandomExecutionTime();
-			virtual_machine[i]=new VMA(i, xCPU_Avaible, xMEM_Avaible, (float)xstart_time, (float)xexecution_time);	
-			virtual_machine[i].printNew();
-		}	*/
-		
-		
 		
 		
 		/**
@@ -68,22 +61,60 @@ public class Testbed {
 			front_agent.subscribeSMA(i+14);
 			//SMAS.put(i+14, SMA_low[i]);
 		}
+		this.run();
 				
 		front_agent.printSMAS();
         //At this point all the SMAS are available
 		
 		//Generates the 300 VMAs
-		myWorkload=new WorkloadGenerator(0,"127.0.0.0",1);
-		/**
-		 * During this execution, every time a new VMAs enter the simulation, the FA should designates a host SMA
-		 * */
+		//We need that the WorkLoadGenerator returns one by one the
+		//VMA,
+		// First choice. Bring the VMA to this code
+		//Second choice. Update the WorkLoadGenerator to be here
+
 		//Generates the 300 VMAs
 	}
+	
+	/*Code added 2016 October 6th**/
+	
+    public void run(){    	
+		int xCPU_Avaible,xMEM_Avaible;
+		long xstart_time=0;
+		long xexecution_time=0;
+		long delay=0;
+		long old_start_time;
+		myWorkload=new WorkloadGenerator();
+		/**
+		 * During this execution, every time a new VMAs enter the simulation, the FA should designates a host SMA
+		 * */		
+		try{
+		for (int i=0;i<300;i++){
+		//public VMA(int xId, int xCPU_Avaible, int xMEM_Avaible, float xstart_time, float xexecution_time)
+		  xCPU_Avaible=myWorkload.generateRandomCPU();
+		  xMEM_Avaible=myWorkload.generateRandomMEM();
+		  old_start_time=xstart_time;
+		  xstart_time=myWorkload.generateRandomStartTime(xstart_time);			
+		  xexecution_time= myWorkload.generateRandomExecutionTime(xexecution_time);
+		  delay=xstart_time-old_start_time;
+		  Thread.sleep(delay);
+		  virtual_machine[i]=new VMA(i, xCPU_Avaible, xMEM_Avaible, (float)xstart_time, (float)xexecution_time);
+		  //Here we need to subscribe the VMA to the FA. Review if the listVMA is better to be a VMA object
+		  //virtual_machine[i].printVMA();
+	}	
+	}catch (Exception ex){
+        System.out.println(ex);
+     }
+		
+    }
+
+
+
+	/*Code added 2016 October 6th**/
 	
 
 	
 	public static void main(String args[]){						
-		Testbed Simulation=new Testbed(0);
+		Testbed Simulation=new Testbed(0,"127.0.0.0",1);
 		Simulation.start();
 	}
 
--- src/WorkloadGenerator.java
+++ src/WorkloadGenerator.java
@@ -3,7 +3,7 @@ import java.util.Random;
 /**
  * Modified by Joel Trejo
  * */
-public class WorkloadGenerator extends Thread {
+public class WorkloadGenerator{
 
 	private VMA[] virtual_machine=new VMA[300];
 
@@ -11,45 +11,12 @@ public class WorkloadGenerator extends Thread {
 	private static final int AVG_INTERARRIVAL_TIME=500;
 	private static final int AVG_INTERDEPARTURE_TIME=35000;
 	
-	private int simulationRun=0;
-    private String dataCenterIP;
-    
-    
-    public WorkloadGenerator (String dataCenterIP, int simulationRun){
-        this.dataCenterIP = dataCenterIP;
-        this.simulationRun=simulationRun;
-        this.run();
-    }    
-    
-    public void run(){    	
-		int xCPU_Avaible,xMEM_Avaible;
-		long xstart_time=0;
-		long xexecution_time=0;
-		long delay=0;
-		long old_start_time;
-		try{
-		for (int i=0;i<300;i++){
-		//public VMA(int xId, int xCPU_Avaible, int xMEM_Avaible, float xstart_time, float xexecution_time)
-		  xCPU_Avaible=generateRandomCPU();
-		  xMEM_Avaible=generateRandomMEM();
-		  old_start_time=xstart_time;
-		  xstart_time=generateRandomStartTime(xstart_time);			
-		  xexecution_time= generateRandomExecutionTime(xexecution_time);
-		  delay=xstart_time-old_start_time;
-		  Thread.sleep(delay);
-		  virtual_machine[i]=new VMA(i, xCPU_Avaible, xMEM_Avaible, (float)xstart_time, (float)xexecution_time);
-		  //Here we need to subscribe the VMA to the FA. Review if the listVMA is better to be a VMA object
-		  virtual_machine[i].printVMA();
-	}	
-	}catch (Exception ex){
-        System.out.println(ex);
-     }
-		
-    }
+	
+    public WorkloadGenerator (){
 
+    }    
 	
-	
-	private int generateRandomCPU(){
+	public int generateRandomCPU(){
 		Random randomGenerator=new Random();
 		int randCPU=1;
 		//1,2,4,6,8
@@ -70,21 +37,21 @@ public class WorkloadGenerator extends Thread {
 		return randCPU;
 	}
 	
-	private int generateRandomMEM(){
+	public int generateRandomMEM(){
 		Random randomGenerator=new Random();
 		int randMEM= randomGenerator.nextInt(21);
 		randMEM++;
 		return randMEM;
 	}
 	
-	private long generateRandomStartTime(long averageDelayForAllocation){
+	public long generateRandomStartTime(long averageDelayForAllocation){
 		// Based on WorkLoadGenerator
 		long delayForAllocation = (long) (AVG_INTERARRIVAL_TIME*(-Math.log(Math.random())));		
 		averageDelayForAllocation=averageDelayForAllocation+delayForAllocation;		
 		return averageDelayForAllocation;
 	}
 	
-	private long generateRandomExecutionTime(long averageDelayForDeallocation){
+	public long generateRandomExecutionTime(long averageDelayForDeallocation){
 		// Based on WorkLoadGenerator
 		// Rate of inter-departure time is 35000 ms (it is 35 seconds)
 		//We construct a function that determines a value from from 30000 to 40000 starting from the arrival time of the previous machine.
